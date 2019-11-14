136c99755f3afa90ebd2e5d71efbd4867a56a194
boaz <boazjames2@yahoo.com>
2019-11-14 22:49:48 +0300

    added responsiveness

--- assets/js/incubation_script.js
+++ assets/js/incubation_script.js
@@ -1,14 +1,14 @@
 $(document).on('ready', function () {
 
     $('.incubation-menu').on('click', function (e) {
-        const nav = $('.academy-nav-sm');
+        const nav = $('.incubation-nav-sm');
         nav.removeClass("d-none");
         nav.addClass("d-flex");
     });
 
-    /*$('.close').on('click', function (e) {
-        const nav = $('.academy-nav-sm');
+    $('.close').on('click', function (e) {
+        const nav = $('.incubation-nav-sm');
         nav.removeClass("d-flex");
         nav.addClass("d-none");
-    });*/
+    });
 });
